6536dda6267f243c6bba71b64e38199936d10dd0
Adnane Hedadja <adhedadja@gmail.com>
2024-07-04 17:43:03 -0400

    feat: messing around with nextjs layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <span>This is the footer!</span>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+function Header() {
+  return (
+    <header>
+      <span>This is the header!</span>
+    </header>
+  );
+}
+
+export default Header;
